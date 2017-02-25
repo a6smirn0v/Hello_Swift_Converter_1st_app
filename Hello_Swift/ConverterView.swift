df572f13f710fd7fdd4c3433235cce74c8b12946
Александр Смирнов <Alex@aleksandrs-macbook-air.local>
2017-02-25 13:21:28 +0300

    Добавлен код (логическая часть)

---
+++ Hello_Swift/ConverterView.swift
@@ -0,0 +1,21 @@
+//
+//  ConverterView.swift
+//  Hello_Swift
+//
+//  Created by Александр Смирнов on 25/02/17.
+//  Copyright © 2017 alex. All rights reserved.
+//
+
+import UIKit
+
+class ConverterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
